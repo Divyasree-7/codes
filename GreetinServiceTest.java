e42e89c8c878987754658b975dffc403da9681dd
Divyasree-7 <divyasree.dammalapati4@gmail.com>
2026-04-08 21:08:22 +0530

    Update GreetinServiceTest.java

--- GreetinServiceTest.java
+++ GreetinServiceTest.java
@@ -1,27 +1,27 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class GreetingServiceTest {
 
     GreetingService g = new GreetingService();
 
     @Test // ✅ PASS
-    void testValidName() {
+    public void testValidName() {
         assertEquals("Hello John", g.greet("John"));
     }
 
     @Test // ✅ PASS
-    void testEmptyName() {
+    public void testEmptyName() {
         assertEquals("Hello Guest", g.greet(""));
     }
 
     @Test // ❌ FAIL
-    void testWrongOutput() {
+    public void testWrongGreeting() {
         assertEquals("Hi John", g.greet("John")); // wrong
     }
 
     @Test // ❌ FAIL
-    void testNullName() {
+    public void testNullName() {
         assertEquals("Hello Null", g.greet(null)); // wrong
     }
 }
