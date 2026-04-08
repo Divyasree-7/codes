d6893704ceebf2110da81314dbc39b193112e8b0
Divyasree-7 <divyasree.dammalapati4@gmail.com>
2026-04-08 21:09:16 +0530

    Update BankAccountTest.java

--- BankAccountTest.java
+++ BankAccountTest.java
@@ -1,33 +1,33 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
     @Test // ✅ PASS
-    void testDeposit() {
+    public void testDeposit() {
         BankAccount acc = new BankAccount(100);
         acc.deposit(50);
-        assertEquals(150, acc.getBalance());
+        assertEquals(150, acc.getBalance(), 0.01);
     }
 
     @Test // ✅ PASS
-    void testWithdraw() {
+    public void testWithdraw() {
         BankAccount acc = new BankAccount(100);
         acc.withdraw(30);
-        assertEquals(70, acc.getBalance());
+        assertEquals(70, acc.getBalance(), 0.01);
     }
 
     @Test // ❌ FAIL
-    void testWrongBalance() {
+    public void testWrongBalance() {
         BankAccount acc = new BankAccount(100);
         acc.deposit(50);
-        assertEquals(120, acc.getBalance()); // wrong
+        assertEquals(120, acc.getBalance(), 0.01); // wrong
     }
 
     @Test // ❌ FAIL
-    void testOverWithdraw() {
+    public void testOverWithdraw() {
         BankAccount acc = new BankAccount(100);
         acc.withdraw(200);
-        assertEquals(0, acc.getBalance()); // wrong
+        assertEquals(0, acc.getBalance(), 0.01); // wrong
     }
 }
