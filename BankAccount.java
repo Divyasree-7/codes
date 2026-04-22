d740b3c81557ad2f6e56bafaf328116a1eb7e8df
Divyasree-7 <divyasree.dammalapati4@gmail.com>
2026-04-23 00:02:10 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -2,21 +2,33 @@ public class BankAccount {
 
     private double balance;
 
-    public BankAccount(double balance) {
-        this.balance = balance;
+    // Constructor
+    public BankAccount(double initialBalance) {
+        if (initialBalance < 0) {
+            balance = 0;
+        } else {
+            balance = initialBalance;
+        }
     }
 
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Withdraw method
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            balance = 0; // prevents negative balance
+        } else if (amount > 0) {
+            balance -= amount;
+        }
+    }
+
+    // Getter
     public double getBalance() {
         return balance;
-    }
-
-    public void deposit(double amount) {
-        if (amount > 0)
-            balance += amount;
-    }
-
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance)
-            balance -= amount;
     }
 }
